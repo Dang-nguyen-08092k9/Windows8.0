cf40e950f46884ed31aca8b1825c68577a078fbc
Dang-nguyen-08092k9 <103374506+Dang-nguyen-08092k9@users.noreply.github.com>
2022-06-19 12:16:16 +0800

    Update Win.sh

--- Win.sh
+++ Win.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y qemu-system-x86-64
 wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
 wget -O win8.0.qcow2 'https://download1326.mediafire.com/8ey9hj62vejg/73df07xabmz648p/win8.0.qcow2'
 
-curl --silent --show-error http://127.0.0.1:5901/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 4G \
   -cpu EPYC \
